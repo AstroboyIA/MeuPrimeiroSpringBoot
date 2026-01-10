4377330e58c92e83223b5f600e6e9f7d4ebb6009
Walisson Paulo <walisson200939@gmail.com>
2026-01-10 19:07:04 -0300

    Primeiro commit.

---
+++ src/test/java/br/com/walissonpaulo/PrimeroSpring/PrimeroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.walissonpaulo.PrimeroSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
