bfcf189cf816a7ae5bcccdbb95b3c121fb0db749
Walisson Paulo <walisson200939@gmail.com>
2026-01-10 20:10:56 -0300

    Decodificando a senha.

--- src/main/java/br/com/walissonpaulo/PrimeroSpring/filter/FilterTaskAuth.java
+++ src/main/java/br/com/walissonpaulo/PrimeroSpring/filter/FilterTaskAuth.java
@@ -1,15 +1,14 @@
 package br.com.walissonpaulo.PrimeroSpring.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -19,10 +18,25 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
-    }
 
+                var authorization = request.getHeader("Authorization");
+
+                var authEncoded = authorization.substring("Basic".length()).trim();
+
+                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+                var authString = new String(authDecode);
+
+
+                String[] credentials = authString.split(":");
+                String username = credentials[0];
+                String password = credentials[1];
+
+                System.out.println("Authorization");
+                System.out.println(username);
+                System.out.println(password);
+
+        filterChain.doFilter(request, response);
     }
 
 }
