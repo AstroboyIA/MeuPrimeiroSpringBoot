a515d2e937c4d679183dfffeec05b116ea8c18e9
Walisson Paulo <walisson200939@gmail.com>
2026-01-11 17:30:29 -0300

    Criando validação de data.

--- src/main/java/br/com/walissonpaulo/PrimeroSpring/task/TaskController.java
+++ src/main/java/br/com/walissonpaulo/PrimeroSpring/task/TaskController.java
@@ -1,8 +1,11 @@
 package br.com.walissonpaulo.PrimeroSpring.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +19,25 @@ public class TaskController {
 
     @Autowired
     private ITaskRepository taskRepository;
-    
+
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
 
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início ou data de termino inválida, a data deve ser maior que a data atual.");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início deve ser menor que a data de termino.");
+        }
+
         TaskModel task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
